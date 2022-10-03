acc18e6daf480a3b12282877600aab3008a63139
Art Manion <zmanion@protonmail.com>
2022-10-03 16:46:32 -0400

    updated

--- cvelib-howto.sh
+++ cvelib-howto.sh
@@ -8,12 +8,33 @@ CVE_USER=zmanion@protonmail.com
 CVE_ORG=Paleozoic
 CVE_API_KEY=
 
+oldUserID=fu@paleozoic.example.com
+oldUserNameFirst=Fu
+oldUserNameLast=Bar
+newUserID=foo@@paleozoic.example.com
+newUserNameFirst=Foo
+newUserNameLast="$oldUserNameLast"
+
+# testing
+oldUserID=fu@bar.net
+oldUserNameFirst=Fum
+oldUserNameLast=Ble
+newUserID=foo@bar.org
+newUserNameFirst=Fume
+newUserNameLast="$oldUserNameLast"
+
+#
+# Store CVE_API_KEYs elsewhere
 # format of file containing API key:
-# ${environment}:${key}
+# ${CVE_ENVIRONMENT}:${CVE_API_KEY}
 # e.g.,
 # test:1234567890abcde
 # production:43434343434343
-cvecreds=~/.cve
+#
+# CVE_ENVIRONMENT must be unique
+#
+
+cveAPIKeys=~/.cve
 
 #
 # Text formatting
@@ -34,7 +55,7 @@ run_wait()
 	${reset}
 	while read -srn1 key; do
 		if [ "$key" = 'r' ]; then
-			$1
+			eval "$1"
 			echo
 			return
 		elif [ "$key" = 's' ]; then
@@ -45,52 +66,57 @@ run_wait()
 	done
 }
 
+clear
 echo "Reading CVE_API_KEY..."
-if [ $(stat -f %u ${cvecreds}) != $(id -u) ]; then
-	echo "$cvecreds not owned by current user."
+if [ $(stat -f %u ${cveAPIKeys}) != $(id -u) ]; then
+	echo "$cveAPIKeys not owned by current user."
 	echo
 	exit 90
 fi
-if [ $(stat -f "%OMp%OLp" ${cvecreds}) != '0600' ]; then
-	echo "$cvecreds file permissions are not 0600."
+if [ $(stat -f "%OMp%OLp" ${cveAPIKeys}) != '0600' ]; then
+	echo "$cveAPIKeys file permissions are not 0600."
 	echo
 	exit 91
 fi
-CVE_API_KEY=$(grep "$CVE_ENVIRONMENT" "$cvecreds" | cut -d: -f2)
+CVE_API_KEY=$(grep "$CVE_ENVIRONMENT" "$cveAPIKeys" | cut -d: -f2)
 sleep 1
 
 clear
 echo "#"
-echo "# 1. Example script and commands to install, configure, and demonstrate cvelib."
+echo "# Example script and commands to install, configure, and demonstrate cvelib."
 echo "#"
 echo "# Press 'r' to run the proposed command, 's' to skip."
 echo "#"
-echo "# You will need the following information:"
+echo "# Configure this script with the following information:"
 echo "#"
 echo "# CVE_ENVIRONMENT"
 echo "# CVE_USER"
 echo "# CVE_ORG"
 echo "# CVE_API_KEY"
 echo "#"
+echo "# This script will clone the cvelib GitHub repository from the current"
+echo "# directory (./cvelib)."
+echo "#"
 pause
 
 #
 # Versions of some things
 #
-echo
+clear
 echo "#"
 echo "# 2. Versions of some things"
 echo "#"
 echo
 run_wait "bash --version"
 run_wait "python -V"
-run_wait "jq -V"
+run_wait "jq -V # (jq is optional)"
 run_wait "git --version"
 pause
 
 #
 # Install cvelib
 #
+clear
 echo "#"
 echo "# 3. Install cvelib"
 echo "#"
@@ -106,18 +132,15 @@ run_wait "python -m venv venv"
 run_wait "source venv/bin/activate"
 run_wait "pip install --upgrade pip"
 run_wait "pip install -e ."
-
 run_wait "which cve"
-
 run_wait "export CVE_ENVIRONMENT="$CVE_ENVIRONMENT""
 run_wait "export CVE_USER="$CVE_USER""
 run_wait "export CVE_ORG="$CVE_ORG""
-run_wait "export CVE_API_KEY="$CVE_API_KEY""
+run_wait "export CVE_API_KEY=************************************"
+export CVE_API_KEY=$CVE_API_KEY
+run_wait "cve --help | less"
 
-run_wait "cve --help"
-pause
-
-echo
+clear
 echo "#"
 echo "# 4. User management"
 echo "#"
@@ -125,57 +148,65 @@ echo
 run_wait "cve org"
 run_wait "cve org users"
 run_wait "cve user create --help"
-
-
-run_wait "cve user create --username fu@example.com --name-first Fu --name-last Bar"
-run_wait "cve user --username fu@example.com"
+run_wait "cve user create --username $oldUserID --name-first $oldUserNameFirst --name-last $oldUserNameLast"
+run_wait "cve user --username $oldUserID"
 run_wait "cve user update --help"
-run_wait "cve user update --username foo@example.com --name-first Foo"
-run_wait "cve user update --username fu@example.com --new-username foo@example.com"
-run_wait "cve user --username foo@example.com"
-run_wait "cve user update --username foo@example.com --add-role ADMIN"
-run_wait "cve user --username foo@example.com"
-run_wait "cve user update --username foo@example.com --remove-role ADMIN"
-run_wait "cve user --username foo@example.com"
-run_wait "cve user update --username foo@example.com --mark-inactive"
-run_wait "cve user --username foo@example.com"
-run_wait "cve user update --username foo@example.com --mark-active"
-run_wait "cve user --username foo@example.com"
-pause
+run_wait "cve user update --username $oldUserID --name-first $newUserNameFirst"
+run_wait "cve user --username $oldUserID"
+run_wait "cve user update --username $oldUserID --new-username $newUserID"
+run_wait "cve user --username $newUserID"
+run_wait "cve user update --username $newUserID --add-role ADMIN"
+run_wait "cve user --username $newUserID"
+run_wait "cve user update --username $newUserID --remove-role ADMIN"
+run_wait "cve user --username $newUserID"
+run_wait "cve user update --username $newUserID --mark-inactive"
+run_wait "cve user --username $newUserID"
+run_wait "cve user update --username $newUserID --mark-active"
+run_wait "cve user --username $newUserID"
 
 echo "#"
 echo "# 5. Reservation"
 echo "#"
 echo
+clear
 run_wait "cve list --help"
+clear
 run_wait "cve list"
+clear
 run_wait "cve list --state rejected"
+clear
 run_wait "cve list --state published"
+clear
 run_wait "cve list --state reserved"
-pause
 
+clear
 echo "N.B. API changes from 1.1 to 2.1 include:"
 echo "  'reject' --> 'rejected'"
 echo "  'public' --> 'published'"
 echo
 pause
-
 reserveTemp=$(mktemp)
 trap 'rm -f "$reserveTemp"' EXIT
 run_wait "cve reserve --raw | tee $reserveTemp" 
-newID=$(cat $reserveTemp | jq -r '.cve_ids[0].cve_id'))
-
+# using jq
+#newID=$(cat $reserveTemp | jq -r '.cve_ids[0].cve_id')
+# using python
+newID=$(cat $reserveTemp | python -c 'import json,sys;cve=json.load(sys.stdin);print(cve["cve_ids"][0]["cve_id"])')
 run_wait "cve list"
 run_wait "cve list | grep $newID"
 
+clear
 echo "#"
 echo "# 5. Publication"
 echo "#"
 echo
 run_wait "cve list"
-run_wait "cve publish $newID --json '{"affected":[{"versions":[{"version":"0","status":"affected","lessThan":"1.0.3","versionType":"semver"}],"product":"Software","vendor":"Example"}],"descriptions":[{"lang":"en","value":"Example Software prior to 1.0.3 has a vulnerability, using cvelib-howto.sh, this is $newID"}],"providerMetadata":{"orgId":"77e550a0-813d-44aa-8a55-a59814101335","shortName":"Paleozoic"},"references":[{"url":"https://www.example.com/security/EA-1234.html","name":"Example Security Advisory EA-1234"}]}'"
-run_wait "cve list --state-published"
-run_wait "cve list --state-published | grep $newID"
+run_wait "cve publish $newID --json '{"affected":[{"versions":[{"version":"0","status":"affected","lessThan":"1.0.3","versionType":"semver"}],"product":"Software","vendor":"Example"}],"descriptions":[{"lang":"en","value":"Example Software prior to 1.0.3 has a vulnerability, using $0, this is $newID."}],"providerMetadata":{"orgId":"77e550a0-813d-44aa-8a55-a59814101335","shortName":"Paleozoic"},"references":[{"url":"https://www.example.com/security/EA-1234.html","name":"Example Security Advisory EA-1234"}]}'"
+run_wait "cve list --state published | grep $newID"
+run_wait "cve show $newID"
+run_wait "cve show --show-record $newID | less"
+run_wait "cve publish $newID --json '{"affected":[{"versions":[{"version":"0","status":"affected","lessThan":"1.0.3","versionType":"semver"}],"product":"Software","vendor":"Example"}],"descriptions":[{"lang":"en","value":"Example Software prior to 1.0.3 has a vulnerability, using $0, this is $newID, with a recent update."}],"providerMetadata":{"orgId":"77e550a0-813d-44aa-8a55-a59814101335","shortName":"Paleozoic"},"references":[{"url":"https://www.example.com/security/EA-1234.html","name":"Example Security Advisory EA-1234"}]}'"
+run_wait "cve show --show-record $newID | less"
 
 deactivate
 echo "Fin"
